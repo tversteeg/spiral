78f1abe90fdce1cdf9f67c4af5d074a62d6088e7
Thomas Versteeg <thomasversteeg@gmx.com>
2017-12-23 11:02:18 +0100

    Create a first not working version of Euclidean distance

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub enum Spiral {
-    Euclidean,
+    Euclidean(EuclideanIterator),
     Manhattan(ManhattanIterator),
     Chebyshev(ChebyshevIterator)
 }
@@ -121,19 +121,118 @@ impl Iterator for ManhattanIterator {
     }
 }
 
+pub struct EuclideanIterator {
+    max_distance: i32,
+    start_x: i32,
+    start_y: i32,
+
+    x: i32,
+    y: i32,
+    length: i32,
+    dir: u8,
+
+    y2_new: i32,
+    y2: i32,
+    ty: i32
+}
+
+impl EuclideanIterator {
+    #[allow(dead_code)]
+    pub fn new(x: i32, y: i32, max_distance: u16) -> Self {
+        // Approximate cos pi/4 by 185363 / 2 ^ 18
+        let length = (max_distance as i32 * 185363) >> 18;
+
+        let diameter = (max_distance * max_distance) as i32;
+
+        EuclideanIterator {
+            max_distance: max_distance as i32,
+            start_x: x,
+            start_y: y,
+
+            x: 0,
+            y: max_distance as i32 - 1,
+            length: length,
+            dir: 0,
+
+            y2: diameter,
+            y2_new: 0,
+            ty: (max_distance as i32 * 2) - 1
+        }
+    }
+}
+
+impl Iterator for EuclideanIterator {
+    type Item = (i32, i32);
+
+    fn next(&mut self) -> Option<(i32, i32)> {
+        let pos = match self.dir {
+            0 => (self.x, self.y),
+            1 => (self.x, -self.y),
+            2 => (-self.x, self.y),
+            3 => (-self.x, -self.y),
+
+            4 => (self.y, self.x),
+            5 => (self.y, -self.x),
+            6 => (-self.y, self.x),
+            7 => (-self.y, -self.x),
+            _ => return None
+        };
+
+        self.dir += 1;
+        if self.dir >= 8 {
+            self.dir = 0;
+
+            self.x += 1;
+            if self.x > self.length {
+                return None;
+            }
+
+            self.y2_new -= (self.x * 2) - 3;
+
+            if self.y2 - self.y2_new >= self.ty {
+                self.y2 -= self.ty;
+                self.y -= 1;
+                self.ty -= 2;
+            }
+        }
+
+        Some((self.start_x + pos.0, self.start_y + pos.1))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn output() {
-        const SIZE: usize = 7;
+        const SIZE: usize = 9;
 
         println!("Manhattan");
         let mut output: [i32; SIZE * SIZE] = [0; SIZE * SIZE];
 
         let mut current = 0;
-        let spiral = ManhattanIterator::new(3, 3, 4);
+        let spiral = ManhattanIterator::new(4, 4, 5);
+        for (x, y) in spiral {
+            current += 1;
+
+            let index = x as usize + (y as usize * SIZE);
+            output[index] = current;
+        }
+
+        for y in 0..SIZE {
+            for x in 0..SIZE {
+                let index = x as usize + (y as usize * SIZE);
+                let output_val = output[index];
+
+                print!("{:3} ", output_val);
+            }
+            println!("");
+        }
+
+        println!("Euclidean");
+        current = 0;
+        let spiral = EuclideanIterator::new(4, 4, 5);
         for (x, y) in spiral {
             current += 1;
 
@@ -153,7 +252,7 @@ mod tests {
 
         println!("Chebyshev");
         current = 0;
-        let spiral = ChebyshevIterator::new(3, 3, 4);
+        let spiral = ChebyshevIterator::new(4, 4, 5);
         for (x, y) in spiral {
             current += 1;
 
@@ -170,7 +269,6 @@ mod tests {
             }
             println!("");
         }
-
     }
 
     #[test]
