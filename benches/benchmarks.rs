a2ccd57e38036df19adcc6752566f68cb9db8a42
Thomas Versteeg <thomasversteeg@gmx.com>
2018-01-03 21:48:51 +0100

    Add benchmarks

---
+++ benches/benchmarks.rs
@@ -0,0 +1,77 @@
+#![feature(test)]
+
+extern crate spiral;
+extern crate test;
+
+use test::Bencher;
+
+use spiral::*;
+
+const SMALL: u16 = 2;
+const MEDIUM: u16 = 10;
+const LARGE: u16 = 100;
+const HUGE: u16 = 1000;
+
+// Small
+#[bench]
+fn chebyshev_small(b: &mut Bencher) {
+    b.iter(|| for _ in ChebyshevIterator::new(0, 0, SMALL) {});
+}
+
+#[bench]
+fn manhattan_small(b: &mut Bencher) {
+    b.iter(|| for _ in ManhattanIterator::new(0, 0, SMALL) {});
+}
+
+#[bench]
+fn euclidean_small(b: &mut Bencher) {
+    b.iter(|| for _ in EuclideanIterator::new(0, 0, SMALL) {});
+}
+
+// Medium
+#[bench]
+fn chebyshev_medium(b: &mut Bencher) {
+    b.iter(|| for _ in ChebyshevIterator::new(0, 0, MEDIUM) {});
+}
+
+#[bench]
+fn manhattan_medium(b: &mut Bencher) {
+    b.iter(|| for _ in ManhattanIterator::new(0, 0, MEDIUM) {});
+}
+
+#[bench]
+fn euclidean_medium(b: &mut Bencher) {
+    b.iter(|| for _ in EuclideanIterator::new(0, 0, MEDIUM) {});
+}
+
+// Large
+#[bench]
+fn chebyshev_large(b: &mut Bencher) {
+    b.iter(|| for _ in ChebyshevIterator::new(0, 0, LARGE) {});
+}
+
+#[bench]
+fn manhattan_large(b: &mut Bencher) {
+    b.iter(|| for _ in ManhattanIterator::new(0, 0, LARGE) {});
+}
+
+#[bench]
+fn euclidean_large(b: &mut Bencher) {
+    b.iter(|| for _ in EuclideanIterator::new(0, 0, LARGE) {});
+}
+
+// Huge
+#[bench]
+fn chebyshev_huge(b: &mut Bencher) {
+    b.iter(|| for _ in ChebyshevIterator::new(0, 0, HUGE) {});
+}
+
+#[bench]
+fn manhattan_huge(b: &mut Bencher) {
+    b.iter(|| for _ in ManhattanIterator::new(0, 0, HUGE) {});
+}
+
+#[bench]
+fn euclidean_huge(b: &mut Bencher) {
+    b.iter(|| for _ in EuclideanIterator::new(0, 0, HUGE) {});
+}
